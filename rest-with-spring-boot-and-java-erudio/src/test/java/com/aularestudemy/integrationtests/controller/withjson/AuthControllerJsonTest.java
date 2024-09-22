4ca8ec4ebba4a311662a2c4a89ea91669ccddd13
Raphael Loureiro <142115140+fkrj021@users.noreply.github.com>
2024-09-22 13:33:09 -0300

    subindo 18 - voltar depois

---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/com/aularestudemy/integrationtests/controller/withjson/AuthControllerJsonTest.java
@@ -0,0 +1,71 @@
+package com.aularestudemy.integrationtests.controller.withjson;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.aularestudemy.configs.TesteConfigs;
+import com.aularestudemy.integrationtests.testcontainers.AbstractIntegrationTeste;
+import com.aularestudemy.integrationtests.vo.AccountCredentialsVO;
+import com.aularestudemy.integrationtests.vo.TokenVO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(OrderAnnotation.class)
+public class AuthControllerJsonTest extends AbstractIntegrationTeste {
+
+	private static TokenVO tokenVO;
+	
+	@Test
+	@Order(1)
+	public void testSignin() throws JsonMappingException, JsonProcessingException {
+		
+		AccountCredentialsVO user = 
+				new AccountCredentialsVO("leandro", "admin123");
+		
+		tokenVO = given()
+				.basePath("/auth/signin")
+					.port(TesteConfigs.SERVER_PORT)
+					.contentType(TesteConfigs.CONTENT_TYPE_JSON)
+				.body(user)
+					.when()
+				.post()
+					.then()
+						.statusCode(200)
+							.extract()
+							.body()
+								.as(TokenVO.class);
+		
+		assertNotNull(tokenVO.getAccessToken());
+		assertNotNull(tokenVO.getRefreshToken());
+	}
+	
+	@Test
+	@Order(2)
+	public void testRefresh() throws JsonMappingException, JsonProcessingException {
+		
+		var newTokenVO = given()
+				.basePath("/auth/refresh")
+				.port(TesteConfigs.SERVER_PORT)
+				.contentType(TesteConfigs.CONTENT_TYPE_JSON)
+					.pathParam("username", tokenVO.getUsername())
+					.header(TesteConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenVO.getRefreshToken())
+				.when()
+					.put("{username}")
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.as(TokenVO.class);
+		
+		assertNotNull(newTokenVO.getAccessToken());
+		assertNotNull(newTokenVO.getRefreshToken());
+	}
+}
